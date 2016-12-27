2dafffc6c5b932371b2f3587e0080e6f7d040652
zopoonho <zopoonho@gmail.com>
2016-12-27 14:33:33 +0800

    My Django Girls app, first commit

---
+++ mysite/trips/views.py
@@ -0,0 +1,28 @@
+# trips/views.py
+
+from datetime import datetime
+from django.shortcuts import render
+
+
+def hello_world(request):
+    return render(request, 'hello_world.html', {
+        'current_time': str(datetime.now()),
+    })
+
+from django.shortcuts import render
+from .models import Post
+
+
+def home(request):
+    post_list = Post.objects.all()
+    return render(request, 'home.html', {
+        'post_list': post_list,
+    })
+
+from django.shortcuts import render
+from .models import Post
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'post.html', {'post': post})    
